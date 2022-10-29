3e52b3c454a22e1d3265bc4d6e9013e83009ec83
amirisback <faisalamircs@gmail.com>
2022-10-29 11:06:35 +0700

    update: SingleThread CI

---
+++ src/main/kotlin/app/SingleThread.kt
@@ -0,0 +1,15 @@
+package app
+
+/**
+ * Created by Faisal Amir on 29/10/22
+ * -----------------------------------------
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * -----------------------------------------
+ * Copyright (C) Frogobox ID / amirisback
+ * All rights reserved
+ */
+
+fun main() {
+    println("Damn You Github Action !!")
+}
