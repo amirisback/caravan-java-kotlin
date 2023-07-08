51468283577724b5f583ce2743763107554ff3bf
amirisback <faisalamircs@gmail.com>
2023-07-08 15:04:26 +0700

    update: Main.java

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,12 +3,19 @@ package app;
 
 import app.playground.Word;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Word word = new Word();
         word.outputWord();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a word: ");
+        String input = scanner.nextLine();
+        System.out.println("You entered: " + input);
 
     }
 
