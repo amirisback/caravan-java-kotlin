1bf201c5017f98ede82421f4856b8fd7169d5362
amirisback <faisalamircs@gmail.com>
2023-07-08 15:10:45 +0700

    update: Main.java

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,7 +3,6 @@ package app;
 
 import app.playground.Word;
 
-import java.util.Scanner;
 
 public class Main {
 
@@ -11,11 +10,6 @@ public class Main {
 
         Word word = new Word();
         word.outputWord();
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a word: ");
-        String input = scanner.nextLine();
-        System.out.println("You entered: " + input);
 
     }
 
