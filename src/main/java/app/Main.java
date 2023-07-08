d99fb35e5d968d4e280d9c136dd1fce19ee2ff5d
amirisback <faisalamircs@gmail.com>
2023-07-08 14:09:54 +0700

    update: add java playground

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+import static app.playground.OuputWord.InnerClass.outputWord;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        outputWord();
+
+    }
+
+}
